2d3348240ff5c64bae9ea3c9b439ef7940dee765
nimbusNuii <ardcsr@gmail.com>
2026-01-09 10:34:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quickstart-line-mini-app-4b9cna6p
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nimbusNuii/quickstart-line-mini-app-4b9cna6p)
---
+++ index.js
@@ -0,0 +1,34 @@
+// Import stylesheets
+import './style.css';
+
+// Body element
+const body = document.querySelector("#body")
+
+// Button elements
+const btnSend = document.querySelector("#btnSend")
+const btnShare = document.querySelector("#btnShare")
+const btnLogIn = document.querySelector("#btnLogIn")
+const btnLogOut = document.querySelector("#btnLogOut")
+const btnScanCode = document.querySelector("#btnScanCode")
+const btnOpenWindow = document.querySelector("#btnOpenWindow")
+const btnShortcut = document.querySelector("#btnShortcut")
+
+// Profile elements
+const email = document.querySelector("#email")
+const userId = document.querySelector("#userId")
+const pictureUrl = document.querySelector("#pictureUrl")
+const displayName = document.querySelector("#displayName")
+const statusMessage = document.querySelector("#statusMessage")
+
+// QR element
+const code = document.querySelector("#code")
+
+// Friendship element
+const friendShip = document.querySelector("#friendShip")
+
+async function main() {
+  // Initialize LIFF SDK
+
+  // Try a function
+}
+main();
